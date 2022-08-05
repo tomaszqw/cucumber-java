bb811fc1c074a03c9a797eaf67015951cc1edde8
TomaszqW <t.wzorek@gmail.com>
2022-08-05 17:14:56 +0200

    project init

---
+++ src/test/java/pl/tomaszqw/seleniumspring/SeleniumSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomaszqw.seleniumspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
